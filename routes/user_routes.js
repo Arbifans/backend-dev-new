1baf28af7fd094d9752fdea430baabf741aa6a30
devDedeo <93327877+Dedeo3@users.noreply.github.com>
2025-12-21 20:25:09 +0700

    update validasi create assest

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createProfile, login, getProfileByWalletAddress, getAssetsWithPayment, verifyAssetsPayment,createAssets, getProfile, getAssets, getAssetsById, getAssetsByIdCreator, updateProfile } from '../controller/user_controller.js';
+import { createProfile, login, getChatWithPayment, verifyChatPayment,getProfileByWalletAddress, getAssetsWithPayment, verifyAssetsPayment,createAssets, getProfile, getAssets, getAssetsById, getAssetsByIdCreator, updateProfile } from '../controller/user_controller.js';
 
 const router = express.Router();
 router.post('/creator/register', createProfile);
@@ -12,8 +12,10 @@ router.get('/creator/assets-creator/:idCreator', getAssetsByIdCreator)
 router.put('/creator/register/:id', updateProfile);
 
 // payment x402
-router.get('/creator/assets/:id/purchase', getAssetsWithPayment);
-router.post('/creator/assets/:id/verify', verifyAssetsPayment);
+// router.get('/creator/assets/:id/purchase', getAssetsWithPayment);
+// router.post('/creator/assets/:id/verify', verifyAssetsPayment);
 
 router.get('/creator/find-by-wallet/:walletAddress', getProfileByWalletAddress);
+
+
 export default router;
